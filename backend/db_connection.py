7778177d27d1577a3cd6887f25f980fa22796af8
Geetha495 <cs20btech11011@iith.ac.in>
2023-03-28 18:06:07 +0530

    added class for student specific operations

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -23,9 +23,10 @@ class Database:
         
         # test the database connection
         self.test_connection()
-
-        # getting the collection from cluster
-        self.collection = self.cluster[configurations['database']['collection']]
+        
+        # Student collection
+        self.studentOperations = StudentDBOperations(self.cluster[configurations["database"]["student_collection"]])
+        
 
     # code to check if remote mongo server is accessible
     def test_connection(self):
@@ -36,22 +37,29 @@ class Database:
             print("Error connecting to MongoDB: " + str(e))
             exit()
 
-    # add user to the database
-    def add_user(self, user):
+class StudentDBOperations():
+    def __init__(self,collection) -> None:
+        self.collection = collection
+        
+    def add_user(self,user):
         self.collection.insert_one(user)
 
     # check if a user if present in the database using email
-    def check_email(self, email):
-        return bool(self.collection.find_one({"email": email}))
+    def check_email(self,email):
+        if self.collection.find_one({"email": email}):
+            return True
+        return False
 
     # check if a user if present in the database using username
-    def check_username(self, username):
-        return bool(self.collection.find_one({"username": username}))
+    def check_username(self,username):
+        if self.collection.find_one({"username": username}):
+            return True
+        return False
 
     # get user from the database using email
-    def get_user_by_email(self, email):
+    def get_user(self,email):
         return self.collection.find_one({"email": email})
 
     # get user from the database using username
-    def get_user_by_username(self, username):
+    def get_user_by_username(self,username):
         return self.collection.find_one({"username": username})
