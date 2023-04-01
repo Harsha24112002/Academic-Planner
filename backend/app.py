e5bdf5f0d5843a2e88f8983276c2ff8ed3954226
Harsha <cs20btech11028@iith.ac.in>
2023-04-01 17:32:41 +0530

    Added admin blueprint

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 import authentication
 import maps
-
+import admin
 app = Flask(__name__)
 app.secret_key = "secret-key"
 
@@ -10,6 +10,6 @@ app.secret_key = "secret-key"
 # The url's whose prefix starts with authentication, goes to this authentication.py
 app.register_blueprint(authentication.authentication)
 app.register_blueprint(maps.maps)
-
+app.register_blueprint(admin.admin)
 if __name__ == '__main__':
 	app.run(debug=True)
